aa65d0358519e612d5dcfd8cac7a61e17047abce
Diego Duran <diegocd887@gmail.com>
2025-08-11 14:31:35 -0600

    simulación y gráfico

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
+import scipy.stats
 import streamlit as st
+import time
 
 st.header('Lanzar una moneda')
 
-# Slider para elegir número de intentos
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+# gráfico inicia en 0.5
+chart = st.line_chart([0.5])
 
-# Botón para ejecutar el experimento
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+    return mean
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
+    mean = toss_coin(number_of_trials)
+    st.write(f'Media final: {mean:.3f}')
 
