dd3980a8f5debfc0af1d2fb64202f470c97afd01
Diego Duran <diegocd887@gmail.com>
2025-08-11 14:21:33 -0600

    agregada funcionalidad con widgets

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
+# Slider para elegir número de intentos
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+
+# Botón para ejecutar el experimento
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
 
